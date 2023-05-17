4dc09b7283f7ffdf1acf0beed775c5d777cb0bcb
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-05-17 13:39:16 +0200

    TODO FIxes

--- Websites/Pentrax/js/carousel.js
+++ Websites/Pentrax/js/carousel.js
@@ -4,37 +4,37 @@ const clientCarousel = document.querySelector("#clients-carousel")
 
 let items = clientCarousel.querySelectorAll('.carousel .carousel-item')
 
-items.forEach((el) => {
+items.forEach((item) => {
     const minPerSlide = 4 // Max number of slides
-    let next = el.nextElementSibling
+    let next = item.nextElementSibling
+
     for (var i = 1; i < minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
             next = items[0]
         }
         let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
+        item.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
     }
 })
 
 
-// CLient Carousel 
-// Need ID Clients-carousel on div with carousel class to work
+// Screenshot Carousel 
+// Need ID screenshot-carousel on div with carousel class to work
 const screenshotCarousel = document.querySelector("#screenshot-carousel")
 
-let items1 = screenshotCarousel.querySelectorAll('.carousel .carousel-item')
+let items1 = screenshotCarousel.querySelectorAll('.carousel .carousel-item') // Save all carousel items in array
 
-items1.forEach((el) => {
+items1.forEach((item) => { //Foreach loop all items in carousel items array
     const minPerSlide = 5 // Max number of slides
-    let next = el.nextElementSibling
+    let next = item.nextElementSibling // Next == next following sibling (Returns NULL if current item is last element)
     for (var i = 1; i < minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
+        if (!next) { // Check if next == null if true change it to first slide resetting the slide
             next = items1[0]
         }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
+        let cloneChild = next.cloneNode(true) // Save the next slide node(With children inside) in cloneChild
+        item.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
     }
 })
